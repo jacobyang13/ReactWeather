e744ff7316ef8af22876af647a4a6cf3de7f7526
jyy <jyy@jyy-pc.local>
2017-06-19 15:52:04 -0400

    Changed about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -4,8 +4,12 @@ var React = require('react');
 var About = (props) =>{
   return (
     <div>
-    <h3>About Component</h3>
+    <h1 className = "text-center">Information</h1>
     <p>I am Jacob Yang testing a page for RISC Networks</p>
+    <p>Here is my company's website</p>
+      <li>
+        <a href="http://www.riscnetworks.com">RISC Networks</a>
+      </li>
   </div>
   )
 };
