3ce92dfbfd0b5d0250b943058b4f81720ee133a8
jyy <jyy@jyy-pc.local>
2017-06-19 14:18:02 -0400

    Change in about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,7 +3,10 @@ var React = require('react');
 
 var About = (props) =>{
   return (
+    <div>
     <h3>About Component</h3>
+    <p>I am Jacob Yang testing a page for RISC Networks</p>
+  </div>
   )
 };
 
