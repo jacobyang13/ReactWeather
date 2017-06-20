30066a572dc304f62408ebd67243cc1e6b533410
jyy <jyy@jyy-pc.local>
2017-06-20 11:21:41 -0400

    Final react weather app

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -4,7 +4,14 @@ var {Link, IndexLink} = require('react-router');
 var Nav = React.createClass({
   onSearch: function(e){
     e.preventDefault();
-    alert('Not yet wired up!');
+
+    var location = this.refs.weatherHold.value;
+    var encodedLocation = encodeURIComponent(location);
+
+    if(location.length > 0){
+      this.refs.weatherHold.value = '';
+      window.location.hash = '#/?location=' + encodedLocation;
+    }
 
   },
   render: function(){
@@ -30,7 +37,7 @@ var Nav = React.createClass({
             <form onSubmit= {this.onSearch}>
               <ul className = "menu">
                 <li>
-                  <input type="search" placeholder="Search weather by city"/>
+                  <input type="search" ref = "weatherHold" placeholder="Search weather by city"/>
                 </li>
                 <li>
                   <input type = "submit" className = "button" value = "Get Weather"/>
